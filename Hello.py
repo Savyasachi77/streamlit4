785dec66d420e2131fdd9eef4ccaa51819c1f695
Siddharth Savyasachi <69157680+Savyasachi77@users.noreply.github.com>
2023-10-05 07:21:10 +0000

    done

--- Hello.py
+++ Hello.py
@@ -1,51 +1,30 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import streamlit as st 
+import pandas as pd
+import numpy as np
 
-import streamlit as st
-from streamlit.logger import get_logger
+# display almost anything
+# st.write
+st.write('Hello World')
 
-LOGGER = get_logger(__name__)
+st.write('Welcome to Streamlit App APIs')
 
+st.write(1234)
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+df = pd.DataFrame({
+    'first_column':[1,2,3,4],
+    'second_column': [10,20,30,40]
+})
 
-    st.write("# Welcome to Streamlit! 👋")
+st.write(df)
 
-    st.sidebar.success("Select a demo above.")
+## display numpy array
+st.write(np.array([1,2,3,4]))
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+## ----------------------------- MAGIC ----------------------------
+st.write("Magic commands")
 
+df1 = pd.DataFrame({'col1':[1,2,3,4]})
 
-if __name__ == "__main__":
-    run()
+df1
+x = 10
+x
